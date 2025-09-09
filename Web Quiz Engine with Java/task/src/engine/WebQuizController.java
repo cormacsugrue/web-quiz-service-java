df8016bf9b6f0fef526c8545c66b152c03668a4f
Cormac <sugrue.cormac@gmail.com>
2025-09-09 11:06:15 +0100

    feat: add post endpoint to verify answer

--- Web Quiz Engine with Java/task/src/engine/WebQuizController.java
+++ Web Quiz Engine with Java/task/src/engine/WebQuizController.java
@@ -1,13 +1,19 @@
 package engine;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class WebQuizController {
     final Quiz quiz;
+    final QuizResponse correctAnswerResponse = new QuizResponse(true, "Congratulations, you're right!");
+    final QuizResponse incorrectAnswerResponse= new QuizResponse(false, "Wrong answer! Please, try again.");
 
     public WebQuizController() {
         String[] options = {"Robot", "Tea leaf", "Cup of coffe", "Bug"};
@@ -18,5 +24,22 @@ public class WebQuizController {
     public Quiz requestQuiz() {
         return quiz;
     }
+
+    @PostMapping("/api/quiz")
+    public ResponseEntity<?> submitAnswer(@RequestParam("answer") int answer) {
+
+        if (answer < 0 || answer >= quiz.getOptions().length) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "The answer provided is out of bounds!"));
+        }
+
+        else if (answer == 2) {
+            return ResponseEntity.ok(correctAnswerResponse);
+        }
+
+        return ResponseEntity.ok(incorrectAnswerResponse);
+
+    }
+
 
 }
